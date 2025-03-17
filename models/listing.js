0249236f8120050ef0efd1f665081b624a76b9f6
Payel647 <mallickkoyel098@gmail.com>
2025-03-17 22:21:42 +0530

    first commit

---
+++ models/listing.js
@@ -0,0 +1,22 @@
+const mongoose=require("mongoose");
+const Schema=mongoose.Schema;
+
+const listingSchema= new Schema(
+    {
+      title:{
+        type:String,
+        require:true,
+      },
+      description: String,
+      image:{
+        type:String,
+        default:"https://images.unsplash.com/photo-1569271304854-c2848dd07200?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fHN1bnNldCUyMHNlYXxlbnwwfHwwfHx8MA%3D%3D",
+        set:(v)=>v===""?"https://images.unsplash.com/photo-1569271304854-c2848dd07200?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fHN1bnNldCUyMHNlYXxlbnwwfHwwfHx8MA%3D%3D":v,
+      },
+      price:Number,
+      location:String,
+      country:String,
+    }
+);
+const Listing=mongoose.model("Listing",listingSchema);
+module.exports=Listing;
