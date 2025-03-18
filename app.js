4aa0655e259aa4e28fca5d5689d82e8014c5ed7a
Payel647 <mallickkoyel098@gmail.com>
2025-03-19 00:42:16 +0530

    some feature add

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const ejsMate=require('ejs-mate');
 const wrapAsync=require("./utils/wrapAsync.js");
 const ExpressError=require("./utils/ExpressError.js");
 const {listingSchema}=require("./schema.js");
+const Review=require("./models/review.js");
 main()
   .then(() => {
     console.log("connected to DB");
@@ -86,7 +87,16 @@ app.delete("/listings/:id", wrapAsync(async(req, res) => {
   console.log(deletedListing);
   res.redirect("/listings");
 }));
-
+//Reviews
+//Post Route
+app.post("/listings/:id/reviews",wrapAsync(async(req,res)=>{
+  let listing=await Listing.findById(req.params.id);
+  let newReview=new Review(req.body.review);
+  listing.reviews.push(newReview);
+  await newReview.save();
+  await listing.save();
+  res.redirect(`/listings/${listing._id}`);
+}));
 // app.get("/testListing", async (req, res) => {
 //   let sampleListing = new Listing({
 //     title: "My New Villa",
